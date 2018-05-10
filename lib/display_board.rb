ff1269fa5bca614fb4cdc6a19d5b285bfb6d3674
Will Carter <willmckenna64@gmail.com>
2018-05-10 14:27:16 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,4 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board(board)
-print "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n"
-end
-board = []
-display_board(board)
+puts lines
