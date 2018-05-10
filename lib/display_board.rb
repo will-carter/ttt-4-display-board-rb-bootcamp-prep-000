b9b533025ab36cb41c3df4f69279f84076658598
Will Carter <willmckenna64@gmail.com>
2018-05-10 14:33:40 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,8 +7,8 @@ lines = "-----------"
 seperator = "|"
 
 puts " #{board}[0] #{seperator} #{board}[1] #{seperator} #{board}[2] "
-puts lines
+puts "#{lines}
 puts " #{board}[3] #{seperator} #{board}[4] #{seperator} #{board}[5] "
-puts lines
+puts "#{lines}
 puts " #{board}[6] #{seperator} #{board}[7] #{seperator} #{board}[8] "
 end
