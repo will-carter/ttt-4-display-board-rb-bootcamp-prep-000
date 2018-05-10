c239e0f1024860676fff7cbbcd612aba106f1b0d
Will Carter <willmckenna64@gmail.com>
2018-05-10 14:39:40 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,3 +8,8 @@ def display_board(board)
   
     puts " #{board[0]} #{seperator} #{board[1]} #{seperator} #{board[2]} "
     puts "#{lines}"
+    puts " #{board[3]} #{seperator} #{board[4]} #{seperator} #{board[5]} "
+    puts "#{lines}"
+    puts " #{board[6]} #{seperator} #{board[7]} #{seperator} #{board[8]} "
+    
+end
