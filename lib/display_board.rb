9138ba927c54cf58adc16befa2ddc5dc864cba88
Will Carter <willmckenna64@gmail.com>
2018-05-10 14:35:16 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,9 +6,9 @@ def display_board(board)
 lines = "-----------"
 seperator = "|"
 
-puts " #{board}[0] #{seperator} #{board}[1] #{seperator} #{board}[2] "
+puts " #{board[0]} #{seperator} #{board[1]} #{seperator} #{board[2]} "
 puts "#{lines}"
-puts " #{board}[3] #{seperator} #{board}[4] #{seperator} #{board}[5] "
+puts " #{board[3]} #{seperator} #{board[4]} #{seperator} #{board[5]} "
 puts "#{lines}"
-puts " #{board}[6] #{seperator} #{board}[7] #{seperator} #{board}[8] "
+puts " #{board[6]} #{seperator} #{board[7]} #{seperator} #{board[8]} "
 end
